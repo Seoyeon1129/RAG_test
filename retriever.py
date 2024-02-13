e8b281c6a910432fe8d910d2a4e70e71b1dfe666
Seoyeon1129 <112914475+Seoyeon1129@users.noreply.github.com>
2024-02-13 08:08:50 +0000

    update

---
+++ retriever.py
@@ -0,0 +1,12 @@
+from konlpy.tag import Okt
+
+class SparseRetriever():
+    def __init__(self, corpus, tokenizer=Okt()):
+        self.tokenizer = tokenizer
+        self.corpus = corpus
+        self.tokenized_corpus = [tokenizer.morphs(text) for text in tqdm(corpus)]
+        self.bm25 = BM25Okapi(self.tokenized_corpus)
+    def retrieve(self, query, k=5):
+        tokenized_query = self.tokenizer.morphs(query)
+        results = self.bm25.get_top_n(tokenized_query, self.corpus, n=k)
+        return results
--- streamlit_refer.py
+++ streamlit_refer.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from loguru import logger
-
 import chardet
 import os
+import openai
+from loguru import logger
 from konlpy.tag import Okt
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredMarkdownLoader, PDFPlumberLoader
@@ -11,9 +11,12 @@ from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_openai import OpenAIEmbeddings
 from rank_bm25 import BM25Okapi
 
+from retriever import SparseRetriever
+from prompt import PROMPT_1
+
 def main():
     st.set_page_config(
-    page_title="DirChat",
+    page_title="History Teller",
     page_icon=":books:")
 
     st.title("_Private Data :red[QA Chat]_ :books:")
@@ -28,24 +31,22 @@ def main():
         st.session_state.processComplete = None
 
     with st.sidebar:
-        uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
     if process:
         if not openai_api_key:
             st.info("Please add your OpenAI API key to continue.")
             st.stop()
-        files_text = get_text(uploaded_files)
-        text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
-     
+        with st.spinner(text='데이터 수집중...'):
+            data = load_data()
+            retriever = SparseRetriever(data)
         st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
-
         st.session_state.processComplete = True
 
     if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant", 
-                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+        st.session_state['messages'] =[{"role": "system", "content": "한국사 관련 질문이 아닐 경우 답변을 거부해."},
+                {"role": "system", "content": "질문에 오류 혹은 잘못된 정보가 있는지 확인하고, 있다면 이것을 지적하고 수정해."},
+               ]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -55,94 +56,94 @@ def main():
 
     # Chat logic
     if query := st.chat_input("질문을 입력해주세요."):
-        st.session_state.messages.append({"role": "user", "content": query})
+        contexts = retriever.retrieve(query)
+        prompt = PROMPT_1.format(query=query, contexts=contexts)
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
         with st.chat_message("user"):
             st.markdown(query)
 
         with st.chat_message("assistant"):
-            chain = st.session_state.conversation
 
             with st.spinner("Thinking..."):
-                result = chain({"question": query})
-                with get_openai_callback() as cb:
-                    st.session_state.chat_history = result['chat_history']
-                response = result['answer']
-                source_documents = result['source_documents']
-
+                response = text_generator(messages, openai_api_key)
                 st.markdown(response)
-                with st.expander("참고 문서 확인"):
-                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
-                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
-                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
-                    
-
 
 # Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text)
-    return len(tokens)
 
-def get_text(docs):
-
-    doc_list = []
-    
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if '.pdf' in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.docx' in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.pptx' in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
-
-        doc_list.extend(documents)
-    return doc_list
-
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
-        length_function=tiktoken_len
-    )
-    chunks = text_splitter.split_documents(text)
+def load_data_md():
+    loader = UnstructuredMarkdownLoader("./data/kohist.md", mode='elements')
+    data_md = loader.load()
+    chunks = []
+    for document in data_md:
+        if len(document.page_content)>30:
+            chunks.append(document)
     return chunks
 
-
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-                                        model_name="jhgan/ko-sroberta-multitask",
-                                        model_kwargs={'device': 'cpu'},
-                                        encode_kwargs={'normalize_embeddings': True}
-                                        )  
-    vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
-
-def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-            llm=llm, 
-            chain_type="stuff", 
-            retriever=vetorestore.as_retriever(search_type = 'mmr', vervose = True), 
-            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
-            get_chat_history=lambda h: h,
-            return_source_documents=True,
-            verbose = True
-        )
-
-    return conversation_chain
+def load_data_2007():
+    with open("./data/kohist_2007.txt", "rb") as f:
+        encoding = chardet.detect(f.read())["encoding"]
+    with open("./data/kohist_2007.txt", encoding=encoding) as f:
+        data_2007 = f.read()
 
 
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=100,
+        chunk_overlap=0,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    documents = text_splitter.create_documents([data_2007])
+    chunks = []
+    
+    for document in documents:
+        chunks.append(document)
+        
+    return chunks
+
+def load_data_pdf(chunk_size=700, chunk_overlap = 100):
+    loaders = []
+    for pdf in os.listdir("./data/pdf"):
+        loaders.append(PDFPlumberLoader(os.path.join("./data/pdf", pdf)))
+        
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    chunks = []
+    for loader in tqdm(loaders):
+        chunks.extend(loader.load_and_split(text_splitter=text_splitter))
+        
+    return chunks                   
+
+def load_data():
+    chunks_md = load_data_md()
+    chunks_07 = load_data_2007()
+    # chunks_pdf = load_data_pdf()
+    chunks_total = chunks_md + chunks_07 # + chunks_pdf
+    filtered_chunks = filter_complex_metadata(chunks_total)
+    filtered_texts = [doc.page_content for doc in filtered_chunks]
+    return filtered_texts
+
+def text_generator(messages, openai_api_key, model="gpt-4", temperature=0):
+    openai.api_key = openai_api_key
+    client = openai.OpenAI()
+    messages = [{"role": "system", "content": "한국사 관련 질문이 아닐 경우 답변을 거부해."},
+                {"role": "system", "content": "질문에 오류 혹은 잘못된 정보가 있는지 확인하고, 있다면 이것을 지적하고 수정해."},
+                {"role": "user", "content": prompt},
+               ]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    answer = response.choices[0].message.content
+    return answer
 
 if __name__ == '__main__':
     main()
