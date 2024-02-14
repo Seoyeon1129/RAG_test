8bf047ea51508714437b24d72653849fa30cde4b
Seoyeon1129 <synkim1129@gmail.com>
2024-02-14 16:01:45 +0900

    ㅇㅇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def main():
             query_3 = st.text_input(label="보기 3")
             query_4 = st.text_input(label="보기 4")
             query_5 = st.text_input(label="보기 5")
-        if question and query_1 and query_2 and query_3 and query_4 and query_5:
+        
             question_full = question + '\n\n' + '1.' + query_1 + '\n' + '2.' + query_2 + '\n' + '3.' + query_3 + '\n' + '4.' + query_4 + '\n' + '5.' + query_5
             st.session_state.messages.append({"role": "user", "content": question_full})
             with st.chat_message("user"):
